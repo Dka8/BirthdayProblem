9b1882fff70d1e9160425712314315253b1a1ead
wave <wave_hos@mail.ru>
2017-09-04 22:46:06 +0300

    reflash main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main(int argv, char* argc[]) {
 
 	auto numberOfPeople = atoi(argc[1]);
 	auto numberOfSimulation = atoi(argc[2]);
-
 	std::cout << numberOfPeople << ' ' << numberOfSimulation << '\n';
 	std::cout.flush();
 }
